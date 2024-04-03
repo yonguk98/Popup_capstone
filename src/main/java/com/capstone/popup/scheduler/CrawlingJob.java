d2a86319b03e752075eeec369eff2bbc0ceb1dd6
yonguk <kimyu9827@naver.com>
2024-04-03 15:51:33 +0900

    feature: setting config for quartz library

--- src/main/java/com/capstone/popup/scheduler/CrawlingJob.java
+++ src/main/java/com/capstone/popup/scheduler/CrawlingJob.java
@@ -1,7 +1,9 @@
 package com.capstone.popup.scheduler;
 
+import com.capstone.popup.ocr.Crawling;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Component;
 @DisallowConcurrentExecution // 중복 실행 방지
 @PersistJobDataAfterExecution
 public class CrawlingJob implements Job {
+    @Autowired
+    private final Crawling crawling;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
