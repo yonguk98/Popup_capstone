d8f190938759f0d61aecb8270bcb412efdb9dfd9
yonguk <kimyu9827@naver.com>
2024-04-03 22:38:58 +0900

    feature: filtering function add

--- src/main/java/com/capstone/popup/ocr/OcrRequest.java
+++ src/main/java/com/capstone/popup/ocr/OcrRequest.java
@@ -1,10 +1,13 @@
 package com.capstone.popup.ocr;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -14,23 +17,28 @@ import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class OcrRequest {
 
+    @Value("${secret.ncp.clova.url}")
+    String clovaApiUrl;
+
+    @Value("${secret.ncp.clova.secret")
+    String secretKey;
+
     public Mono<String> sendRequestToClova(Object jsonBody) {
         HttpClient httpClient = HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS,5000) //timeout 시간 조절
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000) //timeout 시간 조절
                 .responseTimeout(Duration.ofMillis(7000))
                 .doOnConnected(conn ->
                         conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
                                 .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
 
-        // TODO: application 파일로 이동
-        String clovaApiUrl = "https://a515vys7wx.apigw.ntruss.com/custom/v1/26783/358771f2854c34bf294efba41a4cefcd8ebce4146656b32996971365a62b0d95/general";
-        String secretKey = "TkdlYXFMYWRoa3hHVlJPbkVIUGJQV3VOUUVKUmlWR2w=";
         WebClient webClient = WebClient.builder()
                 .baseUrl(clovaApiUrl)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
@@ -38,29 +46,66 @@ public class OcrRequest {
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
 
-        return webClient.post()
+        Mono<String> response = webClient.post()
                 .bodyValue(jsonBody)
                 .retrieve()
                 .bodyToMono(String.class);
+
+        response.subscribe(result -> {
+            filtering(result);
+        });
+
+        return response;
+
     }
 
-    public String makeRequestBodyJson(String url){
+    public List<String> filtering(String json) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(json);
+            StringBuilder sb = new StringBuilder();
+
+            JsonNode imagesNode = rootNode.get("images");
+            if (imagesNode != null && imagesNode.isArray()) {
+                for (JsonNode imageNode : imagesNode) {
+                    JsonNode fieldsNode = imageNode.get("fields");
+                    if (fieldsNode != null && fieldsNode.isArray()) {
+                        for (JsonNode fieldNode : fieldsNode) {
+                            JsonNode inferTextNode = fieldNode.get("inferText");
+                            if (inferTextNode != null) {
+                                String inferText = inferTextNode.asText();
+                                sb.append(" " + inferTextNode.asText());
+//                                System.out.println("Infer Text: " + inferText);
+                            }
+                        }
+                    }
+                }
+            }
+            return Arrays.stream(sb.toString().split("(\\d{1,2}\\.)\\s")).toList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String makeRequestBodyJson(String url) {
         JSONObject body = new JSONObject();
-        body.put("version","V1");
-        body.put("lang","ko");
+        body.put("version", "V1");
+        body.put("lang", "ko");
         body.put("timestamp", LocalDateTime.now());
         body.put("requestId", UUID.fromString("?"));
 
 
         JSONObject image = new JSONObject();
-        image.put("name","");
-        image.put("format","png");
-        image.put("url",url);
+        image.put("name", "");
+        image.put("format", "png");
+        image.put("url", url);
 
         JSONArray imgArr = new JSONArray();
         imgArr.add(image);
 
-        body.put("image",imgArr);
+        body.put("image", imgArr);
 
         return body.toJSONString();
     }
