489ab017aa684216b9a4d9e94c34b546e3e25350
yonguk <kimyu9827@naver.com>
2024-04-03 22:41:56 +0900

    feature: execute developing

--- src/main/java/com/capstone/popup/scheduler/CrawlingJob.java
+++ src/main/java/com/capstone/popup/scheduler/CrawlingJob.java
@@ -1,21 +1,32 @@
 package com.capstone.popup.scheduler;
 
 import com.capstone.popup.ocr.Crawling;
+import com.capstone.popup.ocr.OcrRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Slf4j
 @Component
 @DisallowConcurrentExecution // 중복 실행 방지
 @PersistJobDataAfterExecution
 public class CrawlingJob implements Job {
-    @Autowired
-    private final Crawling crawling;
+
+    private Crawling crawling;
+    private OcrRequest ocrRequest;
 
     @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        JobDataMap jobDataMap = context.getMergedJobDataMap();
+        String accountName = jobDataMap.getString("accountName");
+
+        List<String> urlList = crawling.run(accountName);
+        String jsonBody = ocrRequest.makeRequestBodyJson(urlList.get(1));
+        Mono<String> ocrResponse = ocrRequest.sendRequestToClova(jsonBody);
 
     }
 }
