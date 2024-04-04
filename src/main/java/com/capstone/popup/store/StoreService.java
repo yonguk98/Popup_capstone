2898eb3623fd8442be44ab71cb9d61163f790de8
yonguk <kimyu9827@naver.com>
2024-04-04 16:40:09 +0900

    feature: create service, repository file;

---
+++ src/main/java/com/capstone/popup/store/StoreService.java
@@ -0,0 +1,13 @@
+package com.capstone.popup.store;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StoreService {
+
+    public void registerStore(){
+
+    }
+}
