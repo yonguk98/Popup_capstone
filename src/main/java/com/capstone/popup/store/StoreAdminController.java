1ff2ea123c9f559d424efff2dcfa0e8737ede96b
yonguk <kimyu9827@naver.com>
2024-04-04 18:02:49 +0900

    feature: create store controller for admin, general purpose

--- src/main/java/com/capstone/popup/store/StoreAdminController.java
+++ src/main/java/com/capstone/popup/store/StoreAdminController.java
@@ -1,2 +1,25 @@
-package com.capstone.popup.store;public class StoreAdminController {
+package com.capstone.popup.store;
+
+import com.capstone.popup.global.GlobalResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/store")
+public class StoreAdminController {
+
+    private final StoreService storeService;
+
+    @PostMapping("/create")
+    public void createStore(@RequestBody StoreCreateRequestDto dto){
+
+
+    }
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+    @GetMapping
+    public GlobalResponse test(){
+        return GlobalResponse.of("200","test");
+    }
 }
--- src/main/java/com/capstone/popup/store/StoreController.java
+++ src/main/java/com/capstone/popup/store/StoreController.java
@@ -1,2 +1,30 @@
-package com.capstone.popup.store;public class StoreController {
+package com.capstone.popup.store;
+
+import com.capstone.popup.global.GlobalResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/store")
+public class StoreController {
+
+    private final StoreService storeService;
+
+    @GetMapping
+    public void getAllStore(){
+
+    }
+
+    @GetMapping("/{id}")
+    public void getOneStore(@PathVariable Long id){
+
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/test")
+    public GlobalResponse test(){
+        return GlobalResponse.of("200","test");
+    }
 }
