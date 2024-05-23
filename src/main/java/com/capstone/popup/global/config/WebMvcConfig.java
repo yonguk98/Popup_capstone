22cbde31ce4a0e94c6f109057c61d2699c93a0ea
yonguk <kimyu9827@naver.com>
2024-05-24 05:33:59 +0900

    fix: cors path

--- src/main/java/com/capstone/popup/global/config/WebMvcConfig.java
+++ src/main/java/com/capstone/popup/global/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://popups-deploy.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
